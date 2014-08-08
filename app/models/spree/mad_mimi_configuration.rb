4f40b4d16a60a1d005394ed34d44574e94a2b092
Maxim Gladkov <contact@maximgladkov.com>
2014-08-08 14:48:51 +0300

    add webform_id preference to Mad Mimi configuration

--- app/models/spree/mad_mimi_configuration.rb
+++ app/models/spree/mad_mimi_configuration.rb
@@ -2,5 +2,6 @@ module Spree
   class MadMimiConfiguration < Preferences::Configuration
     preference :access_token,  :string
     preference :refresh_token, :string
+    preference :webform_id,    :integer
   end
 end
