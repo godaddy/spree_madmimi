9d3942127645b12ef5206d17f896438e4cce6d8f
Maxim Gladkov <contact@maximgladkov.com>
2014-08-08 17:00:40 +0300

    change Spree url constant

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -7,7 +7,7 @@ unless defined?(CONSTANTS)
     MAD_MIMI_URL    = "http://madmimi.staging.loudlucy.co"
     MAD_MIMI_API    = MAD_MIMI_URL
 
-    SPREE_URL       = "http://localhost:4000"
+    SPREE_URL       = "https://test-spree-madmimi.herokuapp.com"
     SPREE_AUTH_URL  = "#{SPREE_URL}/auth/madmimi"
   else
     MAD_MIMI_ID     = "1133a46ee7f0aee3c2a33ba10e4eb4ef3a1d1fbedf9866b265a1cfcf2622c42c"
