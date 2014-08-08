ee903c75db65225926aefa1764a85e011d1a9e80
Maxim Gladkov <contact@maximgladkov.com>
2014-08-08 14:48:35 +0300

    add update action to Mad Mimi controller

--- app/controllers/spree/admin/mad_mimi_controller.rb
+++ app/controllers/spree/admin/mad_mimi_controller.rb
@@ -3,4 +3,9 @@ class Spree::Admin::MadMimiController < Spree::Admin::BaseController
   def edit
   end
 
+  def update
+    ::MadMimi.webform_id = params[:webform_id] if ::MadMimi.connected?
+    render :edit
+  end
+
 end
