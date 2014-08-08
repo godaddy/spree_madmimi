695806bc0caf340221517473b407a5f1e24b8be2
Maxim Gladkov <contact@maximgladkov.com>
2014-08-08 18:30:03 +0300

    add flash messages to update

--- app/controllers/spree/admin/mad_mimi_controller.rb
+++ app/controllers/spree/admin/mad_mimi_controller.rb
@@ -5,7 +5,12 @@ class Spree::Admin::MadMimiController < Spree::Admin::BaseController
 
   def update
     ::MadMimi.webform_id = params[:webform_id] if ::MadMimi.connected?
-    render :edit
+    if !::MadMimi.webform_id.zero?
+      flash[:success] = Spree.t('mad_mimi.admin.controller.mad_mimi.update.flash.success')
+    else
+      flash[:notice] = Spree.t('mad_mimi.admin.controller.mad_mimi.update.flash.notice')
+    end
+    redirect_to [:edit, :admin, :mad_mimi]
   end
 
 end
